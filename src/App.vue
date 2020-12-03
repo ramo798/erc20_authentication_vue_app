88adafc0894803d011bc6e20763297b48f9610f1
ramo798 <knht31i@gmail.com>
2020-12-03 14:14:46 +0900

    wallet addressの取得の実装

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>{{ walletAddress }}</p>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: () => ({
+    walletAddress: null,
+  }),
+  mounted() {
+    if (window.ethereum) {
+      this.walletAddress = this.handleEthereum();
+    } else {
+      window.addEventListener("ethereum#initialized", this.handleEthereum, {
+        once: true,
+      });
+      setTimeout(this.handleEthereum, 3000);
+    }
+  },
+  methods: {
+    handleEthereum: () => {
+      const { ethereum } = window;
+      if (ethereum && ethereum.isMetaMask) {
+        console.log("Ethereum successfully detected!");
+        console.log(ethereum.selectedAddress);
+        const myaddress = ethereum.selectedAddress;
+        return myaddress;
+      } else {
+        console.log("Please install MetaMask!");
+      }
+    },
+  },
+};
 </script>
 
 <style>

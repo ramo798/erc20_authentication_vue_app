ada2686ee62a6125c77904bf0793ad9762a08ce2
ramo798 <knht31i@gmail.com>
2020-12-13 01:09:34 +0900

    iohone版のmetamaskにも対応させた

--- src/App.vue
+++ src/App.vue
@@ -26,55 +26,26 @@ export default {
     walletAddress: null,
     howmany: null,
     load: false,
+    sec_load: false,
   }),
   mounted() {
     if (window.ethereum) {
-      this.walletAddress = this.handleEthereum();
+      this.getAccount().then((res) => {
+        this.walletAddress = res;
+        this.getTokenBalance(res)
+          .then((res) => {
+            this.howmany = res;
+          })
+          .finally(() => {
+            this.load = true;
+          });
+      });
     } else {
       window.addEventListener("ethereum#initialized", this.handleEthereum, {
         once: true,
       });
       setTimeout(this.handleEthereum, 3000);
     }
-    const web3 = new Web3();
-    web3.setProvider(
-      new web3.providers.HttpProvider(
-        "https://ropsten.infura.io/v3/674fa8842b484b24987e1267a15f06cc"
-      )
-    );
-    const WALLET_ADDRESS = this.walletAddress;
-    const CONTRACT_ADDRESS = "0xc2fdf34114fccB32dF11f0852F61b9165Ec872fF";
-    let minABI = [
-      {
-        constant: true,
-        inputs: [{ name: "_owner", type: "address" }],
-        name: "balanceOf",
-        outputs: [{ name: "balance", type: "uint256" }],
-        type: "function",
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: "decimals",
-        outputs: [{ name: "", type: "uint8" }],
-        type: "function",
-      },
-    ];
-    const contract = new web3.eth.Contract(minABI, CONTRACT_ADDRESS);
-    contract.methods
-      .balanceOf(WALLET_ADDRESS)
-      .call()
-      .then((res) => {
-        contract.methods
-          .decimals()
-          .call()
-          .then((hes) => {
-            console.log(hes);
-            console.log(res / 10 ** hes);
-            this.howmany = res / 10 ** hes;
-            this.load = true;
-          });
-      });
   },
   methods: {
     handleEthereum: () => {
@@ -87,6 +58,61 @@ export default {
       } else {
         console.log("Please install MetaMask!");
       }
+    },
+    requestAccount: () => {
+      const { ethereum } = window;
+      ethereum.request({ method: "eth_requestAccounts" });
+    },
+    getAccount: async function() {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      // console.log(accounts);
+      return accounts[0];
+    },
+    getTokenBalance: (address) => {
+      const web3 = new Web3();
+      web3.setProvider(
+        new web3.providers.HttpProvider(
+          "https://ropsten.infura.io/v3/674fa8842b484b24987e1267a15f06cc"
+        )
+      );
+      const WALLET_ADDRESS = address;
+      const CONTRACT_ADDRESS = "0xc2fdf34114fccB32dF11f0852F61b9165Ec872fF";
+      let minABI = [
+        {
+          constant: true,
+          inputs: [{ name: "_owner", type: "address" }],
+          name: "balanceOf",
+          outputs: [{ name: "balance", type: "uint256" }],
+          type: "function",
+        },
+        {
+          constant: true,
+          inputs: [],
+          name: "decimals",
+          outputs: [{ name: "", type: "uint8" }],
+          type: "function",
+        },
+      ];
+      const contract = new web3.eth.Contract(minABI, CONTRACT_ADDRESS);
+
+      var balance_result = contract.methods
+        .balanceOf(WALLET_ADDRESS)
+        .call()
+        .then((res) => {
+          var balance = contract.methods
+            .decimals()
+            .call()
+            .then((decimal) => {
+              return res / 10 ** decimal;
+            });
+          return balance;
+        })
+        .then((balance) => {
+          return balance;
+        });
+      return balance_result;
     },
   },
 };

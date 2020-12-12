acabc1939e03b10d99fbe73cc2718b813be48811
ramo798 <knht31i@gmail.com>
2020-12-12 23:16:27 +0900

    基本機能の実装完了

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <p>{{ walletAddress }}</p>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <p>{{ walletAddress }}</p>
+      <p>所持数は {{ howmany }} TCKMです。</p>
+    </div>
+    <div v-if="load">
+      <div v-if="howmany > 0">
+        この文章はトークンを所持している人のみ表示されます。
+      </div>
+      <div v-else>
+        この文章はトークンを所持していない人のみ表示されます。
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Web3 from "web3";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
   data: () => ({
     walletAddress: null,
+    howmany: null,
+    load: false,
   }),
   mounted() {
     if (window.ethereum) {
@@ -26,12 +36,51 @@ export default {
       });
       setTimeout(this.handleEthereum, 3000);
     }
+    const web3 = new Web3();
+    web3.setProvider(
+      new web3.providers.HttpProvider(
+        "https://ropsten.infura.io/v3/674fa8842b484b24987e1267a15f06cc"
+      )
+    );
+    const WALLET_ADDRESS = this.walletAddress;
+    const CONTRACT_ADDRESS = "0xc2fdf34114fccB32dF11f0852F61b9165Ec872fF";
+    let minABI = [
+      {
+        constant: true,
+        inputs: [{ name: "_owner", type: "address" }],
+        name: "balanceOf",
+        outputs: [{ name: "balance", type: "uint256" }],
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "decimals",
+        outputs: [{ name: "", type: "uint8" }],
+        type: "function",
+      },
+    ];
+    const contract = new web3.eth.Contract(minABI, CONTRACT_ADDRESS);
+    contract.methods
+      .balanceOf(WALLET_ADDRESS)
+      .call()
+      .then((res) => {
+        contract.methods
+          .decimals()
+          .call()
+          .then((hes) => {
+            console.log(hes);
+            console.log(res / 10 ** hes);
+            this.howmany = res / 10 ** hes;
+            this.load = true;
+          });
+      });
   },
   methods: {
     handleEthereum: () => {
       const { ethereum } = window;
       if (ethereum && ethereum.isMetaMask) {
-        console.log("Ethereum successfully detected!");
+        // console.log("Ethereum successfully detected!");
         console.log(ethereum.selectedAddress);
         const myaddress = ethereum.selectedAddress;
         return myaddress;
